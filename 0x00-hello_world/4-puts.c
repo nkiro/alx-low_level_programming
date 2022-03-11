b136d9fd494625bc27ef280d125ddd142520bea3
Emily <nkiromuriuki10@gmail.com>
2022-03-11 09:43:26 -0800

    C4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,12 @@
-#!/bin/bash
-|#include <stdio.h>int main(void){puts("\"programming is like building a multilingual puzzle");return (0);}
+#include <stdio.h>
+
+/**
+ * main -entry point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	puts("\"programming is like building a multilingual puzzle");
+
+	return(0);
+}
