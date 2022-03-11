1d974dab6defe6987edfa0256741e1ce1d398e12
Emily <nkiromuriuki10@gmail.com>
2022-03-11 10:41:21 -0800

    C5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 
 /*
- * main - Entry point
+ *  function main - Entry point, first function responsible for starting the execution and termination of a program
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf("with proper grammer, but the outcome is a piece of art,\n");
-	return (0);
-}	
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);}	
