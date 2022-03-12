5f1b677e7644a7bdbe7047e1c3c1b9d6f6984697
Emily <nkiromuriuki10@gmail.com>
2022-03-12 01:39:40 -0800

    C5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,12 +2,11 @@
 
 /*
  *function main - Entry point
- *
+ *int main  void(0)-indicatesfunction will not return any value
  * Return: Always 0 (Success)
  */
-int main(void)
+int main void(0)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 
-	return (0);
-}	
+	return (0); }
