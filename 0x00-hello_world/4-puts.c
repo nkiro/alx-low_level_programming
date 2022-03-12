eddca1598756606bf810f593d355a7cd49638f70
Emily <nkiromuriuki10@gmail.com>
2022-03-12 01:09:38 -0800

    C4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,12 @@
 
 /**
  * main -entry point
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts ("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
-	return (0);
+        return (0);
 } 
