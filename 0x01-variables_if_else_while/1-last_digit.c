c98372c82edadae39638a115bf8ffcbeaac69979
Emily <nkiromuriuki10@gmail.com>
2022-03-14 01:40:41 -0700

    var1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,12 +16,12 @@ int main(void)
 
 	printf("Last digit of %i is %i and is ", n, (n % 10));
 
-	if (( n % 10) == 0)
+	if ((n % 10) == 0)
 	{
 		printf("0\n");
 	}
 	else if ((n % 10) > 5)
-        {
+	{
 		printf("greater than 5\n");
 	}
 	else
