3a163949523c93821352d0f5c1a533b32b8ffa7e
nkiro <nkiromuriuki10@gmail.com>
2022-04-07 21:36:26 +0300

    More malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - Allocates memory of size @b.
+ *
+ * @b: Size of memory to be allocated.
+ *
+ * Return: Pointer to the allocated memory.
+ *	   Incase of failure, normal process termination
+ *	   with status value of 98.
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *mal = malloc(b);
+
+	if (mal == NULL)
+		exit(98);
+
+	return (mal);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,5 @@
+# Malloc, realloc and free
+
+This project explores the usage of malloc, realloc and free.
+
+> All the files are written on ubuntu LTS 20.04 and compiled using gcc with the options `-Wall -pedantic -Werror -Wextra -std=gnu89`
